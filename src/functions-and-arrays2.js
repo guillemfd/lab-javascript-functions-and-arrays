433606f077ce1379536cbd4f51dc837e8414fe11
unknown <guillemfd@gmail.com>
2022-05-25 12:42:54 +0200

    reLabing

---
+++ src/functions-and-arrays2.js
@@ -0,0 +1,25 @@
+//1
+function maxOfTwoNumbers(a, b) {
+    if (a > b) {
+        console.log(`${a}`)
+    } else if (a < b) {
+        console.log(`${b}`)
+    } else {
+        console.log('Both numbers are iqual!')
+    }
+}
+
+maxOfTwoNumbers(8, 2)
+console.log('say hou')
+
+// function maxOfTwoNumbers(numberOne, numberTwo) {
+//     if (numberOne > numberTwo) {
+//       return numberOne;
+//     } else if (numberOne < numberTwo) {
+//       return numberTwo;
+//     } else if (numberOne = numberTwo) {
+//       return numberOne;
+//     } else {
+//       return 'Insert a valid number';
+//     }
+//   }
